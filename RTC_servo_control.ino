48638e3a9f92d2690d091c19da9ad0b256441fb6
ahmadaziz6720 <a715210@gmail.com>
2022-11-24 16:56:38 +0700

    fix string comp

--- RTC_servo_control.ino
+++ RTC_servo_control.ino
@@ -8,10 +8,14 @@ DS3231  rtc(A4, A5);
 #define servoMIN 150
 #define servoMAX 600
 
+String coba = "16:41:00";
+String coba2 = "16:42:00";
+
 void setup() {
   Serial.begin(9600);
   allservo.begin();
   rtc.begin();
+  allservo.setOscillatorFrequency(27000000);
   allservo.setPWMFreq(60);
 
 //  uncomment to set time
@@ -21,7 +25,7 @@ void setup() {
 }
 
 void loop(){
-  if(rtc.getTimeStr() == "04:00:00") {
+  if(String(rtc.getTimeStr()) == String(coba)) {
     for (int servo = 0; servo < 16; servo++ ) {
       allservo.setPWM(servo, 0, 325);
       Serial.println(servo);
@@ -33,7 +37,9 @@ void loop(){
       Serial.print(" -- ");
       Serial.println(rtc.getTimeStr());
     }
-  } if(rtc.getTimeStr() == "08.00.00"){
+    Serial.println("masuk1");
+  }
+  if(String(rtc.getTimeStr()) == String(coba2)){
     for (int servo = 0; servo < 16; servo++ ) {
       allservo.setPWM(servo, 0, servoMIN);
       Serial.println(servo);
@@ -45,10 +51,11 @@ void loop(){
       Serial.print(" -- ");
       Serial.println(rtc.getTimeStr());
     }
+    Serial.println("masuk2");
   }
-  Serial.print(rtc.getDOWStr());
-  Serial.print(" ");
-  Serial.print(rtc.getDateStr());
-  Serial.print(" -- ");
+//  Serial.print(rtc.getDOWStr());
+//  Serial.print(" ");
+//  Serial.print(rtc.getDateStr());
+//  Serial.print(" -- ");
   Serial.println(rtc.getTimeStr());
 }
